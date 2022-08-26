9937acc37b45389dc1c7c596df8886d9c30ea8c0
AditiBalla <88562523+AditiBalla@users.noreply.github.com>
2022-08-26 18:47:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+    clown_nose= loadImage("https://i.postimg.cc/KYn5h3p3/Clown-NOSE-removebg-preview.png");
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video, mL);
+    poseNet.on('pose', gotPoses);
+}
+function mL(){
+    console.log("PoseNet has started");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("Nose x = "+results[0].pose.nose.x);
+        console.log("Nose y = "+results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 40;
+        noseY = results[0].pose.nose.y - 40;
+
+    }
+}
+
+function draw(){
+    image(video, 0,0,300,300);
+    image(clown_nose, noseX, noseY, 80, 80);
+}
+
+
+function take_snapshot(){
+    save('ClownME.png');
+}
+
